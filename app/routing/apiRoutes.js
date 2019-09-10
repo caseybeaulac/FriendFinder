5c1f29e7fea44ff46934435297f53bec2b801505
Casey Beaulac <beaulac.casey@gmail.com>
2019-09-10 13:34:20 -0400

    adding friend

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,16 @@
-var friends = require("../data/friends");
+var friendData = require("../data/friends");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-app.get("/api/friends", function(req, res){
-    res.json(friendData);
-});
+    app.get("/api/friends", function (req, res) {
+        res.json(friendData);
+    });
 
-app.post("/api/friends", function(req, res){
-    
-})
+    app.post("/api/friends", function (req, res) {
+        var newFriend = req.body;
+
+        friendData.push(newFriend);
+        res.json(true);
+
+    })
 }
